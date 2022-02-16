74c74a73646387a5c232d78ab2fdc360e2fe7252
Godo Segundo Sanchez Heredia <69484171+GodoSanchezH@users.noreply.github.com>
2022-02-16 12:21:05 -0500

    v1

---
+++ Projecto1_GPIO_Output/Debug/Core/Src/main.d
@@ -0,0 +1,28 @@
+Core/Src/main.o: ../Core/Src/main.c \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/main.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/stm32f4xx.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/stm32f401xe.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/core_cm4.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/cmsis_version.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/cmsis_compiler.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/cmsis_gcc.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/mpu_armv7.h \
+ E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/system_stm32f4xx.h
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/main.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/stm32f4xx.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/stm32f401xe.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/core_cm4.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/cmsis_version.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/cmsis_compiler.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/cmsis_gcc.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include/mpu_armv7.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc/system_stm32f4xx.h:
---
+++ Projecto1_GPIO_Output/Debug/Core/Src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/main.c \
+../Core/Src/system_stm32f4xx.c 
+
+OBJS += \
+./Core/Src/main.o \
+./Core/Src/system_stm32f4xx.o 
+
+C_DEPS += \
+./Core/Src/main.d \
+./Core/Src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F401xE -c -I"E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/Core/Inc" -I"E:/github/STM32F401RE_Cmsis/Projecto1_GPIO_Output/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o
+
+.PHONY: clean-Core-2f-Src
+
