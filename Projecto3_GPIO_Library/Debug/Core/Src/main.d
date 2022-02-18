3227f78fdaf4302c771fcef2460bd88a9e83dbe2
Godo Segundo Sanchez Heredia <69484171+GodoSanchezH@users.noreply.github.com>
2022-02-18 14:22:56 -0500

    v1

---
+++ Projecto3_GPIO_Library/Debug/Core/Src/main.d
@@ -0,0 +1,34 @@
+Core/Src/main.o: ../Core/Src/main.c \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/main.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/stm32f4xx.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/stm32f401xe.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/core_cm4.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/cmsis_version.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/cmsis_compiler.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/cmsis_gcc.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/mpu_armv7.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/system_stm32f4xx.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/Clock_Config.h \
+ E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/GPIO.h
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/main.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/stm32f4xx.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/stm32f401xe.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/core_cm4.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/cmsis_version.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/cmsis_compiler.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/cmsis_gcc.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/CMSIS/Include/mpu_armv7.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/system_stm32f4xx.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/Clock_Config.h:
+
+E:/github/STM32F401RE_Cmsis/Projecto3_GPIO_Library/Core/Inc/GPIO.h:
